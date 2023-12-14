4c8742112a8ec89c7f8e1a70f0cb170edbdb9345
augustopereiratx <augustopt03@gmail.com>
2023-12-14 04:24:16 -0300

    added pass functionality (much better than bitwarden)

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -510,3 +510,7 @@ c.colors.tabs.pinned.selected.even.bg = '#585b70'
 
 # config.set("colors.webpage.darkmode.enabled", True)
 config.bind('<Escape>', 'mode-leave ;; jseval -q document.activeElement.blur()', mode='insert')
+config.bind('<z><l>', 'spawn --userscript qute-pass')
+config.bind('<z><u><l>', 'spawn --userscript qute-pass --username-only')
+config.bind('<z><p><l>', 'spawn --userscript qute-pass --password-only')
+config.bind('<z><o><l>', 'spawn --userscript qute-pass --otp-only')
